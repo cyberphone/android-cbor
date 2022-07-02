81349b79a0d45f94c7b7526251c1d9d2372fde43
Anders Rundgren <anders.rundgren.net@gmail.com>
2022-07-02 21:30:18 +0200

    Update InstrumentedTest.java

--- app/src/androidTest/java/org/webpki/androidcbordemo/InstrumentedTest.java
+++ app/src/androidTest/java/org/webpki/androidcbordemo/InstrumentedTest.java
@@ -1,6 +1,9 @@
 package org.webpki.androidcbordemo;
 
 import android.content.Context;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -9,6 +12,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.spec.ECGenParameterSpec;
 
 import static org.junit.Assert.*;
 
@@ -25,10 +34,34 @@ public class InstrumentedTest {
         new RawReader(InstrumentationRegistry.getInstrumentation().getTargetContext());
     }
 
+    static String ANDROID_KEYSTORE = "AndroidKeyStore";
+
+    static String KEY_1 = "key-1";
+    static String KEY_2 = "key-2";
+
+    @Test
+    public void attestationPlay() throws Exception {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(
+                KeyProperties.KEY_ALGORITHM_EC, ANDROID_KEYSTORE);
+        kpg.initialize(new KeyGenParameterSpec.Builder(
+                KEY_1, KeyProperties.PURPOSE_SIGN)
+                .setAlgorithmParameterSpec(new ECGenParameterSpec("secp256r1"))
+                .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+                .setAttestationChallenge("hello world".getBytes("utf-8"))
+                .build());
+
+        KeyPair keyPair = kpg.generateKeyPair();
+
+        KeyStore keyStore = KeyStore.getInstance(ANDROID_KEYSTORE);
+        keyStore.load(null);
+
+        for (Certificate certificate : keyStore.getCertificateChain(KEY_1)) {
+            Log.i("ATT", certificate.toString());
+        }
+    }
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.webpki.androidcbordemo", appContext.getPackageName());
+        assertEquals("org.webpki.androidcbordemo", RawReader.appContext.getPackageName());
     }
 }
