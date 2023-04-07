82a6e6b3f84a30b21c94007955c8014ec5e60382
Anders Rundgren <anders.rundgren.net@gmail.com>
2023-04-07 19:44:41 +0200

    base64 upgrade

---
+++ app/src/main/java/org/webpki/util/Base64URL.java
@@ -0,0 +1,71 @@
+/*
+ *  Copyright 2006-2021 WebPKI.org (http://webpki.org).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.webpki.util;
+
+/**
+ * Encodes/decodes base64URL data.
+ * See RFC 4648 Table 2.
+ */
+public class Base64URL {
+    
+    private Base64URL() {}  // No instantiation please
+
+    /**
+     * Converts a base64url String to a byte array.
+     * <p>
+     * This method <b>does not</b> accept padding.
+     * </p>
+     *
+     * @param base64url Encoded data
+     * @return Decoded data as a byte array
+     */
+    public static byte[] decode(String base64url) {
+        if (base64url.contains("=")) {
+            throw new IllegalArgumentException("Padding not allowed");
+        }
+        return android.util.Base64.decode(base64url, android.util.Base64.URL_SAFE);
+    }
+
+    /**
+     * Converts a base64url String to a byte array.
+     * <p>
+     * This method accepts <i>optional</i> padding.
+     * </p>
+     *
+     * @param base64url Encoded data
+     * @return Decoded data as a byte array
+     */
+    public static byte[] decodePadded(String base64url) {
+        return android.util.Base64.decode(base64url, android.util.Base64.URL_SAFE);
+    }
+
+    /**
+     * Converts a byte array to a base64url String.
+     * <p>
+     * This method adds no padding.
+     * </p>
+     *
+     * @param byteArray Binary data
+     * @return Encoded data as a String
+     */
+    public static String encode(byte[] byteArray) {
+        return android.util.Base64.encodeToString(byteArray,
+                                                  android.util.Base64.URL_SAFE |
+                                                    android.util.Base64.NO_PADDING |
+                                                    android.util.Base64.NO_WRAP);
+    }
+}
