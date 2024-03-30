bf51dfb6bf73ac1c5c69afe2388a700fbe82504a
Anders Rundgren <anders.rundgren.net@gmail.com>
2024-03-30 08:37:28 +0100

    Jdoc warnings

--- jlibcbor/src/main/java/org/webpki/cbor/CBORObject.java
+++ jlibcbor/src/main/java/org/webpki/cbor/CBORObject.java
@@ -498,11 +498,7 @@ public abstract class CBORObject implements Cloneable {
      * This method requires that the object is a
      * {@link CBORTag}, otherwise a {@link CBORException} is thrown.
      * </p>
-     * <p>
-     * Note that the CBOR <code>bignum</code> type is dealt with
-     * as a specific primitive, in spite of being a tagged object.
-     * </p>
-
+     * 
      * @return CBOR <code>tag</code> object
      */
     public CBORTag getTag() {
@@ -615,7 +611,7 @@ public abstract class CBORObject implements Cloneable {
             int i = inputStream.read();
             if (i < 0) {
                 if (sequenceFlag && atFirstByte) {
-                    return MT_NULL;
+                    return 0;
                 }
                 eofError();
             }
